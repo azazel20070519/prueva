d7e2778a3b8b6e9d9264f256eed81de26903cdab
REYES <153043081+MESIASREYES@users.noreply.github.com>
2024-01-04 18:47:39 -0600

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GataNina-Li/GataBot-MD
+RUN git clone ttps://github.com/MESIASREYES/BOT-C.D.R
 
-WORKDIR /root/GataBot-MD
+WORKDIR /root/BOT-C.D.R
 
-COPY ./root/GataBot-MD
+COPY ./root/BOT-C.D.R
 
 RUN yarn install
 
